cfd55c5d093a84c536e39c2b7b063321ed961e64
Cornel Widmer <cornel.widmer@pixelarium.ch>
2022-05-18 22:10:33 +0200

    added font awesome and new fonts

--- source/public/scripts/index.js
+++ source/public/scripts/index.js
@@ -3,23 +3,12 @@ const createDialog = document.querySelector('div#app-createDialog');
 const todos = document.querySelector('div#app-todos');
 
 function toggleVisibilty(element) {
-    if (element.classList.contains('show')) {
-        element.classList.add('hide')
-        element.classList.remove('show');
-    } else {
-        element.classList.add('show')
-        element.classList.remove('hide');
-    }
+    element.classList.toggle('hide');
 }
 
 // button: theme switcher
 document.querySelector('button#app-actions-switchTheme').addEventListener('click', () => {
-    const body = document.querySelector('body');
-    if (body.classList.contains('dark')) {
-        body.classList.remove('dark');
-    } else {
-        body.classList.add('dark')
-    }
+    document.querySelector('body').classList.toggle('dark');
 });
 
 // button: create new
